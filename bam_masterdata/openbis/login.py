ae2f1272ea4e5d49cfc313d1b88f0f11e04fa589
jpizarro <jose.pizarro-blanco@bam.de>
2025-01-07 14:44:29 +0100

    Fixed ologin

--- bam_masterdata/openbis/login.py
+++ bam_masterdata/openbis/login.py
@@ -3,8 +3,14 @@ from pybis import Openbis
 
 
 # Connect to openBIS
-def ologin():
-    url = environ("URL")
+def ologin() -> Openbis:
+    """
+    Connect to openBIS using the credentials stored in the environment variables.
+
+    Returns:
+        Openbis: Openbis object for the specific openBIS instance defined in `URL`.
+    """
+    url = environ('URL')
     o = Openbis(url)
-    o.login(environ("USERNAME"), environ("PASSWORD"), save_token=True)
+    o.login(environ('USERNAME'), environ('PASSWORD'), save_token=True)
     return o
