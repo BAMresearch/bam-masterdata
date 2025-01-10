fa99c33aa8be21deb33b217f258188e0db4372bd
jpizarro <jose.pizarro-blanco@bam.de>
2025-01-10 16:33:39 +0100

    Added to_openbis for PropertyTypeDef
    Added to_openbis for entities

---
+++ bam_masterdata/metadata/_maps.py
@@ -0,0 +1,39 @@
+"""
+This module contains the maps for the metadata entities from their labels in this package to the
+ones in openBIS when using PyBIS.
+
+Last update: 2025-01-10
+"""
+
+PROPERTY_TYPE_MAP = {
+    "code": "code",
+    "description": "description",
+    "property_label": "label",
+    "data_type": "dataType",
+    "vocabulary_code": "vocabulary",
+    "metadata": "metaData",
+}
+
+COLLECTION_TYPE_MAP = {
+    "code": "code",
+    "description": "description",
+    "validation_script": "validationPlugin",
+}
+
+DATASET_TYPE_MAP = {
+    "code": "code",
+    "description": "description",
+    "validation_script": "validationPlugin",
+}
+
+OBJECT_TYPE_MAP = {
+    "code": "code",
+    "description": "description",
+    "validation_script": "validationPlugin",
+}
+
+VOCABULARY_TYPE_MAP = {
+    "code": "code",
+    "description": "description",
+    "validation_script": "validationPlugin",
+}
