474b50af9b4943e0ad214639130a1a4f7f10929b
jpizarro <jose.pizarro-blanco@bam.de>
2025-02-18 12:07:55 +0100

    Changed name to get_property_metadata
    Added h5py to dependencies
    
    Added to_json, to_dict, to_hdf5 to BaseEntity
    
    Added testing for to_json, to_dict, to_hdf5

--- tests/metadata/test_entities.py
+++ tests/metadata/test_entities.py
@@ -1,3 +1,6 @@
+import io
+
+import h5py
 import pytest
 
 from bam_masterdata.metadata.definitions import PropertyTypeAssignment
@@ -37,6 +40,33 @@ class TestBaseEntity:
         assert repr(entity) == "MockedEntity()"
         entity.name = "Test"
         assert repr(entity) == "MockedEntity(name='Test')"
+
+    def test_to_json(self):
+        """Test the method `to_json` from the class `BaseEntity`."""
+        entity = generate_base_entity()
+        entity.name = "Test"
+        data = entity.to_json()
+        assert data == '{"name": "Test"}'
+
+    def test_to_dict(self):
+        """Test the method `to_dict` from the class `BaseEntity`."""
+        entity = generate_base_entity()
+        entity.name = "Test"
+        data = entity.to_dict()
+        assert data == {"name": "Test"}
+
+    def test_to_hdf5(self):
+        """Test the method `to_hdf5` from the class `BaseEntity`."""
+        entity = generate_base_entity()
+        entity.name = "Test"
+        # mocking the HDF5 file
+        with h5py.File(io.BytesIO(), "w") as hdf_file:
+            entity.to_hdf5(hdf_file=hdf_file)
+            data = hdf_file
+            assert isinstance(data, h5py.File)
+            assert isinstance(data["MockedEntity"], h5py.Group)
+            assert data["MockedEntity"]["name"][()] == b"Test"
+            assert data["MockedEntity"]["name"][()].decode() == "Test"
 
     def test_model_to_json(self):
         """Test the method `model_to_json` from the class `BaseEntity`."""
