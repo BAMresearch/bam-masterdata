745797d8f2c9fe3505ee2adb9d5b8bf95734f8f7
carlosmada22 <carlosmadariagaaramendi@gmail.com>
2025-02-26 12:35:56 +0100

    Fixed mypy

--- bam_masterdata/checker/checker.py
+++ bam_masterdata/checker/checker.py
@@ -41,7 +41,7 @@ class MasterdataChecker:
         Uses the default datamodel directory unless overridden.
         """
         self.logger.info(f"Loading current data model from: {self.datamodel_dir}")
-        entities_dict = EntitiesDict(python_path=self.source_dir, logger=self.logger)
+        entities_dict = EntitiesDict(python_path=self.datamodel_dir, logger=self.logger)
         self.current_model = entities_dict.single_json()
 
     def load_new_entities(self, source: str, source_type: str):
