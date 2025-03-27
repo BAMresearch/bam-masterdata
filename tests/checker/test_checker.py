7a44d9f2d01de743941575303b2ff1fec9345b34
carlosmada22 <carlosmadariagaaramendi@gmail.com>
2025-03-27 14:46:55 +0100

    Deleted empty test file

--- tests/checker/test_checker.py
+++
@@ -1,5 +0,0 @@
-from bam_masterdata.checker import MasterdataChecker
-
-checker = MasterdataChecker()
-# checker.load_current_model()  # Automatically finds Python files in the datamodel directory
-# print(checker.current_model)
