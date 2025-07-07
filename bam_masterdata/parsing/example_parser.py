de5fedc8d9c4eb56dc1d73832abb2b1e4cddb3ed
Lucas Zimmermann <lucas.zimmermann@bam.de>
2025-07-07 10:53:56 +0200

    added click function and example_parser

---
+++ bam_masterdata/parsing/example_parser.py
@@ -0,0 +1,7 @@
+from bam_masterdata.parsing.parsing import AbstractParser
+
+class MyParser1(AbstractParser):
+    def parse(self, files: list[str], collection):
+        for file in files:
+            # Logic to parse the file and add data to the collection
+            print(f"Parsing {file} and adding to {collection}")
